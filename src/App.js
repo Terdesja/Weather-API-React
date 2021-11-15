75d037258367c0e8ac5b47e36933be7784e4bdbc
Terdesja <gerfranmarquez@gmail.com>
2021-11-14 23:51:40 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Page from './components/Page';
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <Page />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+
+import Header from '../Header';
+import Form from '../Form';
+import Error from '../Error';
+import Forecast from '../Forecast';
+import Loader from '../Loader';
+
+import useForecast from '../../hooks/useForecast';
+
+import styles from './Page.module.css'
+
+const Page = () => {
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
+
+    const onSubmit = (value) => {
+        //call fn here
+        submitRequest(value)
+    }
+
+    return (
+        <Fragment>
+            <Header />
+            <div className={`${styles.box} position-relative`}>
+                
+                {/* Form */}
+                {!isLoading && <Form submitSearch={onSubmit}/>}
+                {/* Error */}
+                {isError && <Error message={isError} />}
+                {/* Loader */}
+                {isLoading && <Loader/>}
+            </div>
+                {/* Forecast */}
+                {forecast && <Forecast/>}
+
+            
+        </Fragment>
+    );
+};
+
+export default Page;
